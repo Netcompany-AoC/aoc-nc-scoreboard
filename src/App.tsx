76a7a2e26f6ae1a93cf59d88c6b860be61585c1c
Thomas Feld <tfeld00@gmail.com>
2023-11-20 10:39:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
         <span className="privboard-days">
           {days.map((day, idx) => {
             // eslint-disable-next-line jsx-a11y/anchor-is-valid
-            return day ? <a href="" className={""+(idx+1) === selectedDay ? 'bold':''} onClick={(ev) => {
+            return day ? <a href="" className={""+(idx+1) === selectedDay ? 'leaderboard-daylinks-selected':''} onClick={(ev) => {
               ev.preventDefault()
               ev.stopPropagation();
               setSelectedDay(""+(idx+1));
